d43bc038cb1b3d0b77bf753ae16d7e034ab77f05
leejunhyuk <jh_lee@g.kmou.ac.kr>
2021-08-27 06:31:27 +0000

    update model

--- VIT-base/model.py
+++ VIT-base/model.py
@@ -9,15 +9,11 @@ class vision_transformer(nn.Module):
     """ Vision Transformer model
     classifying input images (x) into classes
     """
-    def __init__(self, in_channel: int = 3, img_size:int = 224, 
-                 patch_size: int = 16, emb_dim:int = 16*16*3, 
-                 n_enc_layers:int = 15, num_heads:int = 3, 
-                 forward_dim:int = 4, dropout_ratio: float = 0.2, 
-                 n_classes:int = 18):
+    def __init__(self, n_classes:int = 18):
         super().__init__()
 
         model = ViT('B_16_imagenet1k', pretrained=True)
-        model.fc = nn.Linear(768, 18, bias = True)
+        model.fc = nn.Linear(768, n_classes, bias = True)
 
     def forward(self, x):
         x = self.model(x)
