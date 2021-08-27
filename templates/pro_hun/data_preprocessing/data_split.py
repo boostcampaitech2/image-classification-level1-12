2cc06b09be48eff7a5aef8725ff2a2092bb78c5d
HeoJunghun <herjh0405@naver.com>
2021-08-27 13:16:39 +0000

    [code revise] util py update and data_split split+fold_num & ensure_dir

--- templates/pro_hun/data_preprocessing/data_split.py
+++ templates/pro_hun/data_preprocessing/data_split.py
@@ -1,12 +1,13 @@
 import os
 import random
 import shutil
-from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import tqdm
 from sklearn.model_selection import StratifiedKFold
+
+from utils.util import ensure_dir
 
 random_seed = 12
 np.random.seed(random_seed)
@@ -26,17 +27,17 @@ class Run_Split:
         df (pd.DataFrame): train_df와 val_df
         df_name (str): 입력된 df에 맞춰서 입력
         """
-        dirname = Path(os.path.join(self.dirname, df_name))
-        if not dirname.is_dir():
-            dirname.mkdir(parents=True, exist_ok=False)
+        dirname = os.path.join(self.dirname, df_name)
+        # 빈 directory 생성
+        ensure_dir(dirname)
 
         for idx in range(len(df)):
             path = df["path"][idx]
             name = df["name"][idx]
             shutil.copy(path, os.path.join(dirname, name))
 
-    def train_val_split(self, train_label):
-        KFold = StratifiedKFold(5)
+    def train_val_split(self, train_label, fold_num):
+        KFold = StratifiedKFold(fold_num)
         X = train_label.index
         y = train_label["label"]
 
@@ -54,8 +55,9 @@ if __name__ == "__main__":
     train_path = "/opt/ml/image-classification-level1-12/templates/data/train"
     train_label = pd.read_csv(os.path.join(train_path, "train_with_label.csv"))
     run_train = Run_Split(os.path.join(train_path, "image_all"))
-    train_list, val_list = run_train.train_val_split(train_label)
+    fold_num = 5
+    train_list, val_list = run_train.train_val_split(train_label, fold_num)
 
-    for i in tqdm.tqdm(range(5)):
+    for i in tqdm.tqdm(range(fold_num)):
         run_train.image_all(train_list[i], f"train{i}_image")
         run_train.image_all(val_list[i], f"val{i}_image")
