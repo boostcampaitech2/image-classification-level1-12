a4a389f7338e68eb250c306725203fa423317e40
jodong2 <whgusehd96@gmail.com>
2021-08-31 09:07:31 +0000

    focal loss

--- loss/jodong2/loss.py
+++ loss/jodong2/loss.py
@@ -1,9 +1,31 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch
+from torch.autograd import Variable
 
 def nll_loss(output, target):
     return F.nll_loss(output, target)
 
 
 cross_entropy = nn.CrossEntropyLoss()
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        ce_loss = nn.CrossEntropyLoss()(inputs, targets)
+
+        pt = torch.exp(-ce_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
+
+focalloss = FocalLoss()
