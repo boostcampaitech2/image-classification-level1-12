67ddb8ab30faef0272c7b138f068bd10eff12216
HeoJunghun <herjh0405@naver.com>
2021-09-02 17:24:49 +0000

    [move folder] pro_hun to root directory

---
+++ pro_hun/model/metric.py
@@ -0,0 +1,17 @@
+import torch
+from sklearn.metrics import f1_score
+
+
+# 한 Batch에서의 Accuracy 값
+def batch_acc(pred, label):
+    return torch.sum(pred == label)
+
+
+# 한 Batch에서의 f1_score 값
+def batch_f1(pred, label, method):
+    return f1_score(pred, label, average=method)
+
+
+# Epoch당 평균값 반환
+def epoch_mean(val, len):
+    return val / len
---
+++ pro_hun/model/model.py
@@ -0,0 +1,34 @@
+import math
+
+import torch.nn as nn
+
+
+def resnet_finetune(model, classes):
+    """
+    resnet model명과 output class 개수를 입력해주면
+    그것에 맞는 모델을 반환, pretrained, bias initialize
+    """
+
+    model = model(pretrained=True)
+    # Freeze 해제
+    # for params in model.parameters():
+    #     params.requires_grad = False
+    model.fc = nn.Linear(in_features=512, out_features=classes, bias=True)
+
+    print("네트워크 필요 입력 채널 개수", model.conv1.weight.shape[1])
+    print("네트워크 출력 채널 개수 (예측 class type 개수)", model.fc.weight.shape[0])
+
+    nn.init.xavier_uniform_(model.fc.weight)
+    stdv = 1.0 / math.sqrt(model.fc.weight.size(1))
+    model.fc.bias.data.uniform_(-stdv, stdv)
+
+    # layer 깊게 쌓기
+    # model.fc = nn.Linear(in_features=512, out_features=128, bias=True)
+    # model.bc = nn.BatchNorm2d(
+    #     128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True
+    # )
+    # model.relu = nn.ReLU(inplace=True)
+    # model.dropout = nn.Dropout(p=0.2)
+    # model.fc2 = nn.Linear(in_features=128, out_features=classes, bias=True)
+
+    return model
